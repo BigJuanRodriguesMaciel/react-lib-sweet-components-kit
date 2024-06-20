d02270e92787316f577f9e18aae746524f58f43e
Juan Maciel <juanrodriguesmaciel1@gmail.com>
2024-06-20 23:16:08 +0100

    🏷️ feat: types and const files

--- src/@types/commun.ts
+++ src/@types/commun.ts
@@ -1,9 +1,8 @@
 type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>
+	? Acc[number]
+	: Enumerate<N, [...Acc, Acc['length']]>;
 
-type RangeNumbers<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
+type RangeNumbers<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
+type Hexadecimal = `#${string}`;
 
-export type {
-    RangeNumbers
-}
+export type { RangeNumbers, Hexadecimal };
