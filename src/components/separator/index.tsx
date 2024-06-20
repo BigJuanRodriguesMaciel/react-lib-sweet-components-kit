9b2db41b52005f942d0bb89b36fe3ece1d49918c
Juan Maciel <juanrodriguesmaciel1@gmail.com>
2024-06-20 23:20:42 +0100

    :sparkles: feat: separator component

--- src/components/separator/index.tsx
+++ src/components/separator/index.tsx
@@ -1,5 +1,5 @@
-import React, { ComponentProps, FC } from "react";
+import React, { ComponentProps, FC } from 'react';
 
-export const Separator: FC<ComponentProps<"span">> = () => {
-    return <span className="border-radius-15 w-85 h-7 bg-green display-block"/>
-}
+export const Separator: FC<ComponentProps<'span'>> = () => {
+	return <span className='border-radius-15 w-85 h-7 bg-green display-block' />;
+};
