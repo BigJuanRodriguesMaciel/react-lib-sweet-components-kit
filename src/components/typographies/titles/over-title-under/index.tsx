b240424cdeebe53d150625c858608bca60503fc0
Juan Maciel <juanrodriguesmaciel1@gmail.com>
2024-06-20 23:21:03 +0100

    :sparkles: feat: OverTitleUnder component

--- src/components/typographies/titles/over-title-under/index.tsx
+++ src/components/typographies/titles/over-title-under/index.tsx
@@ -1,19 +1,23 @@
-import React, { FC, ReactNode } from "react";
-import { IDefaultCustomProps } from "./types";
-import { Separator } from "../../../separator";
+import React, { FC, ReactNode } from 'react';
+import { IDefaultCustomProps } from './types';
+import { Separator } from '../../../separator';
 
 export const OverTitleUnder: FC<IDefaultCustomProps> = ({ tag, texts, highlight }) => {
-    const generateText = (text:string, highlightIndex:number) => {
-        return text.split(' ').map((text, index) => {
-            return highlightIndex === index ? <strong className="fg-black">{text}{' '}</strong> : <>{text}{' '}</>
-        })
-    }
-    return (
-        <>
-            <tag.over className="font-size-14 poppins-bold">{texts.over}</tag.over>
-            <tag.title className="font-size-60 fg-white poppins-bold">{generateText(texts.title, highlight.title).map((e: ReactNode) => e)}</tag.title>
-            <Separator />
-            <tag.under className="font-size-42 fg-white poppins-bold">{generateText(texts.under, highlight.under).map((e: ReactNode) => e)}</tag.under>
-        </>
-    )
-}
+	const generateText = (text: string, highlightIndex: number) => {
+		return text.split(' ').map((text, index) => {
+			return highlightIndex === index ? <strong className='fg-black'>{text} </strong> : <>{text} </>;
+		});
+	};
+	return (
+		<>
+			<tag.over className='font-size-14 poppins-bold'>{texts.over}</tag.over>
+			<tag.title className='font-size-60 fg-white poppins-bold'>
+				{generateText(texts.title, highlight.title).map((e: ReactNode) => e)}
+			</tag.title>
+			<Separator />
+			<tag.under className='font-size-42 fg-white poppins-bold'>
+				{generateText(texts.under, highlight.under).map((e: ReactNode) => e)}
+			</tag.under>
+		</>
+	);
+};
--- src/components/typographies/titles/over-title-under/types.ts
+++ src/components/typographies/titles/over-title-under/types.ts
@@ -1,20 +1,17 @@
 interface IOverTitleUnder<T> {
-    over: T,
-    title: T,
-    under: T,
+	over: T;
+	title: T;
+	under: T;
 }
 
 interface IDefaultCustomProps {
-    colors: {
-        primary: string,
-        secondary: string,
-    },
-    texts: IOverTitleUnder<string>,
-    tag: IOverTitleUnder<keyof HTMLElementTagNameMap>,
-    highlight: IOverTitleUnder<number>
+	colors: {
+		primary: string;
+		secondary: string;
+	};
+	texts: IOverTitleUnder<string>;
+	tag: IOverTitleUnder<keyof HTMLElementTagNameMap>;
+	highlight: IOverTitleUnder<number>;
 }
 
-export type {
-    IOverTitleUnder,
-    IDefaultCustomProps
-}
+export type { IOverTitleUnder, IDefaultCustomProps };
