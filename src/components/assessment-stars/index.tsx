898c26d8f8601f946aa85078c914b9e56fbae7b4
Juan Maciel <juanrodriguesmaciel1@gmail.com>
2024-10-12 19:41:32 +0100

    :recycle: refactor: AssessmentStars

--- src/components/assessment-stars/index.tsx
+++ src/components/assessment-stars/index.tsx
@@ -3,7 +3,6 @@ import { DefaultCustomProps } from './types';
 import { Star } from 'react-lib-icons-component';
 import './styles.modules.scss';
 import { COLORS } from '../../_const';
-import { Hexadecimal } from 'src/@types/common';
 
 export const AssessmentStars: FC<DefaultCustomProps> = ({ assessmentIndex }) => {
 	return (
@@ -13,7 +12,7 @@ export const AssessmentStars: FC<DefaultCustomProps> = ({ assessmentIndex }) => 
 				.map((_, index) => (
 					<li>
 						<Star
-							stroke={COLORS.MAIN_PINK as Hexadecimal}
+							stroke={COLORS.MAIN_PINK}
 							fill={index + 1 <= assessmentIndex ? COLORS.MAIN_PINK : '#fff'}
 							height={'24px'}
 							width={'24px'}
